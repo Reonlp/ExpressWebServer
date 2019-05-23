4cd8be1923370369793696dd384a976e815101b3
Sergio Leon <leonsergio0011@gmail.com>
2019-05-24 00:25:53 +0200

    Moving from serving static html to serving views in a dinamic way with a temple engline

--- src/app.js
+++ src/app.js
@@ -1,19 +1,34 @@
 const path = require('path');
 const express = require('express');
 
-console.log(__dirname);
-console.log();
-
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+//HANDLEBARS TEMPLATE ENGINE
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
 
 app.get('/', (req, res) => {
-  res.send('<h1> ARRIBA ESPAÑA </h1>');
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Leon leon'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Andrew Mead'
+  });
 });
 
 
+app.get('/help', (req, res) => {
+  res.render('help', {
+    helpText: 'This is some helpful text.',
+    name: ''
+  })
+});
 
 app.get('/weather', (req, res) => {
   res.send({
