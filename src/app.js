09464009fdb2d483d7837cb71e7a7e7dbcc7596d
Sergio Leon <leonsergio0011@gmail.com>
2019-05-24 12:16:49 +0200

    Templates updated (handlebars package)

--- src/app.js
+++ src/app.js
@@ -1,42 +1,77 @@
-const path = require('path');
-const express = require('express');
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
 
-const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public');
+const app = express()
 
-//HANDLEBARS TEMPLATE ENGINE
-app.set('view engine', 'hbs');
-app.use(express.static(publicDirectoryPath));
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../views/templates')
+const partialsPath = path.join(__dirname, '../views/partials')
 
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Weather App',
-    name: 'Leon leon'
-  });
-});
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Sergio León'
+    })
+})
 
 app.get('/about', (req, res) => {
-  res.render('about', {
-    title: 'About Me',
-    name: 'Andrew Mead'
-  });
+    res.render('about', {
+        title: 'About Me',
+        name: 'Sergio León'
+    })
+})
+
+app.get('/about/*', (req, res) => {
+  res.render('error', {
+    title: 'Error',
+    message: 'About article not found',
+    name: 'Sergio León'
+  })
 });
 
-
 app.get('/help', (req, res) => {
-  res.render('help', {
-    helpText: 'This is some helpful text.',
-    name: ''
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Sergio León'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+  res.render('error', {
+    title: 'Error',
+    message: 'Help article not found'
   })
 });
 
 app.get('/weather', (req, res) => {
-  res.send({
-    forecast: 'It is snowing',
-    location: 'Philadelphia'
-  });
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    })
+})
+
+app.get('/weather/*', (req, res) => {
+  res.render('error', {
+    title: 'Error',
+    message: 'Weather article not found'
+  })
 });
 
+app.get('*', (req, res) => {
+  res.send("My 404 page");
+})
+
 app.listen(3000, () => {
-  console.log('Server is up on port 3000');
-});
+    console.log('Server is up on port 3000.')
+})
